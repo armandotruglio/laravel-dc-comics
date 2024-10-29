a9214eeb50afc03f19c229c1ba71e233cc9326e4
armandotruglio <armando.truglio1@gmail.com>
2024-10-29 18:51:09 +0100

    Added store method to Source Controller

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -29,6 +29,22 @@ class ComicsController extends Controller
      */
     public function store(Request $request)
     {
+        $formData = $request->all();
+
+
+        $comic = new Comics();
+        $comic->title = $formData["title"];
+        $comic->description = $formData["description"];
+        $comic->thumb = $formData["thumb"];
+        $comic->price = $formData["price"];
+        $comic->series = $formData["series"];
+        $comic->sale_date = $formData["sale_date"];
+        $comic->type = $formData["type"];
+        $comic->artists = $formData["artists"];
+        $comic->writers = $formData["writers"];
+        $comic->save();
+
+        return redirect()->route("comics.show", [ "id" => $comic->id]);
 
     }
 
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,5 +3,60 @@
 @section("page-title", "Inserisci Nuovo Fumetto")
 
 @section("main-content")
+<div class="container">
+    <div class="row justify-content-around">
+        <form class="col-12 col-md-6 card m-3" method="POST" action="{{ route("comics.store") }}">
+            @csrf
+
+            <h1 class="mb-3">
+                Aggiungi un fumetto:
+            </h1>
+            <div class="mb-3">
+                <label for="comic-title" class="form-label">Titolo:</label>
+                <input type="text" class="form-control" id="comic-title" name="title">
+            </div>
+            <div class="mb-3">
+                <label for="comic-description" class="form-label">Descrizione:</label>
+                <input type="text" class="form-control" id="comic-description" name="description">
+            </div>
+            <div class="mb-3">
+                <label for="comic-thumb" class="form-label">Copertina:</label>
+                <input type="text" class="form-control" id="comic-thumb" name="thumb">
+            </div>
+            <div class="mb-3">
+                <label for="comic-price" class="form-label">Prezzo:</label>
+                <input type="text" class="form-control" id="comic-price" name="price">
+            </div>
+            <div class="mb-3">
+                <label for="comic-series" class="form-label">Serie:</label>
+                <input type="text" class="form-control" id="comic-series" name="series">
+            </div>
+            <div class="mb-3">
+                <label for="comic-sale-date" class="form-label">Data Vendita:</label>
+                <input type="text" class="form-control" id="comic-sale_-ate" name="sale_date">
+            </div>
+            <div class="mb-3">
+                <label for="comic-type" class="form-label">Tipo:</label>
+                <input type="text" class="form-control" id="comic-type" name="type">
+            </div>
+            <div class="mb-3">
+                <label for="comic-artists" class="form-label">Artista:</label>
+                <input type="text" class="form-control" id="comic-artists" name="artists">
+            </div>
+            <div class="mb-3">
+                <label for="comic-writers" class="form-label">Scrittore:</label>
+                <input type="text" class="form-control" id="comic-writers" name="writers">
+            </div>
+            <div class="mb-3 d-flex justify-content-center align-items-center">
+                <button type="submit" class="btn btn-primary me-3">
+                    Aggiungi
+                </button>
+                <button type="reset" class="btn btn-warning me-3">
+                    Reset
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
 
 @endsection
